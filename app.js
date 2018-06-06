bf0ed8ecae4989cc893afb7d12107879143af48e
SuperDizor <felixdion25@hotmail.com>
2018-06-06 00:46:53 -0400

    updated files & gitingnore

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const Discord = require("discord.js");
-
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
 const client = new Discord.Client();
-
-// Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+
+
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
